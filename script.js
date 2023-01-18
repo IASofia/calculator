f6ff460fa0851f26ccdb02ef9137a473e92a6af3
sofia <sofiisfernandes@gmaill.com>
2023-01-18 08:05:09 -0300

    Create function to call the operation based on the operator

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function operate(operator, firstOperand, secondOperand) {
+  if (operator === "divide" && secondOperand === 0)
+    return "Error, cannot divide by 0!";
+
+  switch (operator) {
+    case "add":
+      add(firstOperand, secondOperand);
+      break;
+    case "subtract":
+      subtract(firstOperand, secondOperand);
+      break;
+    case "multiply":
+      multiply(firstOperand, secondOperand);
+      break;
+    case "divide":
+      divide(firstOperand, secondOperand);
+      break;
+    default:
+      return "Operator not found";
+  }
+}
